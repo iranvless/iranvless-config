e3b4d2b812cf6dc38b69a519440abf660dd6d2c7
iranvless <143781323+iranvless@users.noreply.github.com>
2023-09-01 11:28:15 +0330

    Update install.sh

--- deprecated/sniproxy/install.sh
+++ deprecated/sniproxy/install.sh
@@ -3,5 +3,5 @@ apt install sniproxy -y
 systemctl disable sniproxy
 kill -9 `lsof -t -i:443`
 kill -9 `lsof -t -i:80`
-ln -sf $(pwd)/hiddify-sniproxy.service /etc/systemd/system/hiddify-sniproxy.service
-systemctl enable hiddify-sniproxy.service
+ln -sf $(pwd)/iranvless-sniproxy.service /etc/systemd/system/iranvless-sniproxy.service
+systemctl enable iranvless-sniproxy.service

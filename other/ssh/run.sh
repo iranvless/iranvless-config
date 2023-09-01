250a2aa8889c5e61011e5bea1e4e97b4966fb71c
iranvless <143781323+iranvless@users.noreply.github.com>
2023-09-02 00:02:21 +0330

    Update run.sh

--- other/ssh/run.sh
+++ other/ssh/run.sh
@@ -1,6 +1,6 @@
 
 
 sed -i "s/:2222/:$ssh_server_port/g" .env
-ln -sf $(pwd)/hiddify-ssh-liberty-bridge.service /etc/systemd/system/hiddify-ssh-liberty-bridge.service
-systemctl enable hiddify-ssh-liberty-bridge
-systemctl restart hiddify-ssh-liberty-bridge
+ln -sf $(pwd)/iranvless-ssh-liberty-bridge.service /etc/systemd/system/iranvless-ssh-liberty-bridge.service
+systemctl enable iranvless-ssh-liberty-bridge
+systemctl restart iranvless-ssh-liberty-bridge

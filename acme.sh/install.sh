83e57e928cde37a9cc6f4d53a13a1fe7021f3030
iranvless <143781323+iranvless@users.noreply.github.com>
2023-09-01 09:55:03 +0330

    Update install.sh

--- acme.sh/install.sh
+++ acme.sh/install.sh
@@ -1,11 +1,11 @@
 sudo apt-get install -y socat
 sudo apt-get remove -y certbot
-rm -rf /opt/hiddify/
+rm -rf /opt/iranvless/
 
-mkdir -p /opt/hiddify-config/acme.sh/lib/
-curl -L https://get.acme.sh| sh -s -- home /opt/hiddify-config/acme.sh/lib \
-    --config-home /opt/hiddify-config/acme.sh/lib/data \
-    --cert-home  /opt/hiddify-config/acme.sh/lib/certs
+mkdir -p /opt/iranvless-config/acme.sh/lib/
+curl -L https://get.acme.sh| sh -s -- home /opt/iranvless-config/acme.sh/lib \
+    --config-home /opt/iranvless-config/acme.sh/lib/data \
+    --cert-home  /opt/iranvless-config/acme.sh/lib/certs
 
 sed -i 's|_sleep_overload_retry_sec=$_retryafter|_sleep_overload_retry_sec=$_retryafter; if [[ "$_retryafter" > 20 ]];then return 10; fi|g' lib/acme.sh
 ./lib/acme.sh --register-account -m my@example.com

ec7da0b33fb76fcbb11e8c06490cb84e924738ec
iranvless <143781323+iranvless@users.noreply.github.com>
2023-09-01 09:48:28 +0330

    Update install.sh

--- hiddify-panel/install.sh
+++ hiddify-panel/install.sh
@@ -1,21 +1,21 @@
-systemctl kill hiddify-admin.service >/dev/null 2>&1
-systemctl disable hiddify-admin.service >/dev/null 2>&1
-#userdel -f hiddify-panel 2>&1
-useradd -m hiddify-panel -s /bin/bash >/dev/null 2>&1
-chown -R hiddify-panel:hiddify-panel /home/hiddify-panel/ >/dev/null 2>&1
-su hiddify-panel -c localectl set-locale LANG=C.UTF-8 >/dev/null 2>&1
-su hiddify-panel -c update-locale LANG=C.UTF-8 >/dev/null 2>&1
+systemctl kill iranvless-admin.service >/dev/null 2>&1
+systemctl disable iranvless-admin.service >/dev/null 2>&1
+#userdel -f iranvless-panel 2>&1
+useradd -m iranvless-panel -s /bin/bash >/dev/null 2>&1
+chown -R iranvless-panel:iranvless-panel /home/iranvless-panel/ >/dev/null 2>&1
+su iranvless-panel -c localectl set-locale LANG=C.UTF-8 >/dev/null 2>&1
+su iranvless-panel -c update-locale LANG=C.UTF-8 >/dev/null 2>&1
 
-chown -R hiddify-panel:hiddify-panel . >/dev/null 2>&1
+chown -R iranvless-panel:iranvless-panel . >/dev/null 2>&1
 # apt install -y python3-dev
-for req in pip3 uwsgi python3 hiddifypanel lastversion jq sqlite3mysql; do
+for req in pip3 uwsgi python3 iranvlesspanel lastversion jq sqlite3mysql; do
     which $req >/dev/null 2>&1
     if [[ "$?" != 0 ]]; then
         apt --fix-broken install -y
         apt update
         apt install -y python3-pip jq python3-dev
         pip3 install pip
-        pip3 install -U hiddifypanel lastversion uwsgi "requests<=2.29.0" sqlite3-to-mysql
+        pip3 install -U hiranvlesspanel lastversion uwsgi "requests<=2.29.0" sqlite3-to-mysql
         break
     fi
 done
@@ -24,39 +24,39 @@ done
 
 sed -i '/SQLALCHEMY_DATABASE_URI/d' app.cfg
 MYSQL_PASS=$(cat ../other/mysql/mysql_pass)
-echo "SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://hiddifypanel:$MYSQL_PASS@127.0.0.1/hiddifypanel'" >>app.cfg
+echo "SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://hiddifypanel:$MYSQL_PASS@127.0.0.1/iranvlesspanel'" >>app.cfg
 
-# if [ -f hiddifypanel.db ]; then
-#     sqlite3mysql -f hiddifypanel.db -d hiddifypanel -u hiddifypanel -h 127.0.0.1 --mysql-password $MYSQL_PASS
-#     mv hiddifypanel.db hiddifypanel.db.old
+# if [ -f iranvlesspanel.db ]; then
+#     sqlite3mysql -f iranvlesspanel.db -d iranvlesspanel -u iranvlesspanel -h 127.0.0.1 --mysql-password $MYSQL_PASS
+#     mv iranvlesspanel.db hiranvlesspanel.db.old
 # fi
 
 # ln -sf $(which gunicorn) /usr/bin/gunicorn
 
-#pip3 --disable-pip-version-check install -q -U hiddifypanel
+#pip3 --disable-pip-version-check install -q -U iranvlesspanel
 # pip uninstall -y hiddifypanel
-# pip --disable-pip-version-check install -q -U git+https://github.com/hiddify/HiddifyPanel
+# pip --disable-pip-version-check install -q -U git+https://github.com/iranvless/iranvlessPanel
 
 # ln -sf $(which gunicorn) /usr/bin/gunicorn
 ln -sf $(which uwsgi) /usr/local/bin/uwsgi >/dev/null 2>&1
-# hiddifypanel init-db
-ln -sf $(pwd)/hiddify-panel.service /etc/systemd/system/hiddify-panel.service
-systemctl enable hiddify-panel.service
+# iranvlesspanel init-db
+ln -sf $(pwd)/iranvless-panel.service /etc/systemd/system/iranvless-panel.service
+systemctl enable iranvless-panel.service
 if [ -f "../config.env" ]; then
-    su hiddify-panel -c "hiddifypanel import-config -c $(pwd)/../config.env"
+    su iranvless-panel -c "iranvlesspanel import-config -c $(pwd)/../config.env"
     if [ "$?" == 0 ]; then
         rm -f config.env
         # echo "temporary disable removing config.env"
     fi
 fi
 systemctl daemon-reload >/dev/null 2>&1
-echo "*/1 * * * * root $(pwd)/update_usage.sh" >/etc/cron.d/hiddify_usage_update
+echo "*/1 * * * * root $(pwd)/update_usage.sh" >/etc/cron.d/iranvless_usage_update
 service cron reload >/dev/null 2>&1
 
-systemctl start hiddify-panel.service
-# systemctl status hiddify-panel.service --no-pager > /dev/null 2>&1
+systemctl start iranvless-panel.service
+# systemctl status iranvless-panel.service --no-pager > /dev/null 2>&1
 
-echo "0 */6 * * * hiddify-panel $(pwd)/backup.sh" >/etc/cron.d/hiddify_auto_backup
+echo "0 */6 * * * iranvless-panel $(pwd)/backup.sh" >/etc/cron.d/iranvless_auto_backup
 service cron reload
 
 ##### download videos

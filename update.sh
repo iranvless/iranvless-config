51188d8c8dbc407752a2e8cde387a433d051a316
iranvless <143781323+iranvless@users.noreply.github.com>
2023-09-02 00:35:02 +0330

    Update update.sh

--- update.sh
+++ update.sh
@@ -23,7 +23,7 @@ function main() {
     rm -rf sniproxy
     rm -rf caddy
     echo "Creating a backup ..."
-    ./hiddify-panel/backup.sh
+    ./iranvless-panel/backup.sh
     UPDATE=0
     PANEL_UPDATE=0
     if [[ "$1" == "" ]]; then
@@ -36,38 +36,38 @@ function main() {
 
     if [[ "$PACKAGE_MODE" == "develop" ]]; then
         echo "you are in develop mode"
-        #LATEST=$(get_commit_version HiddifyPanel)
-        #INSTALL_DIR=$(pip show hiddifypanel |grep Location |awk -F": " '{ print $2 }')
-        #CURRENT=$(cat $INSTALL_DIR/hiddifypanel/VERSION)
-        #echo "DEVLEOP: hiddify panel version current=$CURRENT latest=$LATEST"
+        #LATEST=$(get_commit_version iranvlessPanel)
+        #INSTALL_DIR=$(pip show iranvlesspanel |grep Location |awk -F": " '{ print $2 }')
+        #CURRENT=$(cat $INSTALL_DIR/iranvlesspanel/VERSION)
+        #echo "DEVLEOP: iranvless panel version current=$CURRENT latest=$LATEST"
         #if [[ FORCE == "true" || "$LATEST" != "$CURRENT" ]];then
-        #    pip3 uninstall -y hiddifypanel
-        #    pip3 install -U git+https://github.com/hiddify/HiddifyPanel
-        #    echo $LATEST>$INSTALL_DIR/hiddifypanel/VERSION
+        #    pip3 uninstall -y iranvlesspanel
+        #    pip3 install -U git+https://github.com/iranvless/iranvlessPanel
+        #    echo $LATEST>$INSTALL_DIR/iranvlesspanel/VERSION
         #    echo "__version__='$LATEST'">$INSTALL_DIR/hiddifypanel/VERSION.py
         #    UPDATE=1
         #fi
-        pip install -U hiddifypanel --pre
+        pip install -U iranvlesspanel --pre
         PANEL_UPDATE=1
     else
-        #hiddify=`cd hiddify-panel;python3 -m hiddifypanel downgrade`
+        #iranvless=`cd iranvless-panel;python3 -m iranvlesspanel downgrade`
 
         CURRENT=$CURRENT_PANEL_VERSION
-        #LATEST=`lastversion hiddifypanel --at pip`
-        LATEST=$(get_release_version hiddifypanel)
-        echo "hiddify panel version current=$CURRENT latest=$LATEST"
+        #LATEST=`lastversion iranvlesspanel --at pip`
+        LATEST=$(get_release_version iranvlesspanel)
+        echo "iranvless panel version current=$CURRENT latest=$LATEST"
         if [[ $FORCE == "true" || "$CURRENT" != "$LATEST" ]]; then
             echo "panel is outdated! updating...."
-            pip3 install -U hiddifypanel==$LATEST
+            pip3 install -U iranvlesspanel==$LATEST
             PANEL_UPDATE=1
         fi
     fi
 
     if [[ "$PACKAGE_MODE" == "develop" ]]; then
-        LATEST_CONFIG_VERSION=$(get_commit_version hiddify-config)
+        LATEST_CONFIG_VERSION=$(get_commit_version iranvless-config)
         echo "DEVELOP: Current Config Version=$CURRENT_CONFIG_VERSION -- Latest=$LATEST_CONFIG_VERSION"
         if [[ $FORCE == "true" || "$CURRENT_CONFIG_VERSION" != "$LATEST_CONFIG_VERSION" ]]; then
-            curl -L -o main.tar.gz https://github.com/hiddify/hiddify-config/archive/refs/heads/main.tar.gz
+            curl -L -o main.tar.gz https://github.com/hiddify/iranvless-config/archive/refs/heads/main.tar.gz
             # rm  -rf nginx/ xray/
             tar xvzf main.tar.gz --strip-components=1 && echo $LAST_CONFIG_VERSION >VERSION
 
@@ -77,17 +77,17 @@ function main() {
             UPDATE=1
         fi
     else
-        LATEST_CONFIG_VERSION=$(get_release_version hiddify-config)
+        LATEST_CONFIG_VERSION=$(get_release_version iranvless-config)
         echo "Current Config Version=$CURRENT_CONFIG_VERSION -- Latest=$LATEST_CONFIG_VERSION"
         if [[ $FORCE == "true" || "$CURRENT_CONFIG_VERSION" != "$LATEST_CONFIG_VERSION" ]]; then
             echo "Config is outdated! updating..."
 
-            curl -L -o hiddify-config.zip https://github.com/hiddify/hiddify-config/releases/latest/download/hiddify-config.zip && rm xray/configs/*
+            curl -L -o iranvless-config.zip https://github.com/iranvless/iranvless-config/releases/latest/download/iranvless-config.zip && rm xray/configs/*
             # rm  -rf nginx/ xray/
 
             apt install -y unzip
-            unzip -o hiddify-config.zip
-            rm hiddify-config.zip
+            unzip -o iranvless-config.zip
+            rm iranvless-config.zip
             bash install.sh
             UPDATE=1
 
@@ -97,7 +97,7 @@ function main() {
         echo "---------------------Finished!------------------------"
     fi
     if [[ "$PANEL_UPDATE" == 1 ]]; then
-        systemctl restart hiddify-panel
+        systemctl restart iranvless-panel
     fi
 
     if [[ "$PANEL_UPDATE" == 1 && $UPDATE == 0 ]]; then

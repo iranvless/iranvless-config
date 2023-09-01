74dd8beef22ae2b56450e9273059fb39b3e5d19d
iranvless <143781323+iranvless@users.noreply.github.com>
2023-09-01 11:21:17 +0330

    Update utils.sh

--- common/utils.sh
+++ common/utils.sh
@@ -1,26 +1,26 @@
 function get_commit_version() {
-    COMMIT_URL=$(curl -s https://api.github.com/repos/hiddify/$1/git/refs/heads/main | jq -r .object.url)
+    COMMIT_URL=$(curl -s https://api.github.com/repos/iranvless/$1/git/refs/heads/main | jq -r .object.url)
     VERSION=$(curl -s $COMMIT_URL | jq -r .committer.date)
     echo ${VERSION:5:11}
 }
 
 function get_release_version() {
-    # COMMIT_URL=https://api.github.com/repos/hiddify/$1/releases/latest
+    # COMMIT_URL=https://api.github.com/repos/iranvless/$1/releases/latest
     # VERSION=$(curl -s --connect-timeout 1 $COMMIT_URL | jq -r .tag_name)
-    VERSION=$(curl -sI https://github.com/hiddify/$1/releases/latest | grep -i location | rev | awk -F/ '{print $1}' | rev)
+    VERSION=$(curl -sI https://github.com/iranvless/$1/releases/latest | grep -i location | rev | awk -F/ '{print $1}' | rev)
     echo ${VERSION//v/}
 }
 
 function get_installed_panel_version() {
-    echo $(pip3 freeze | grep hiddifypanel | awk -F"==" '{ print $2 }')
+    echo $(pip3 freeze | grep iranvlesspanel | awk -F"==" '{ print $2 }')
 }
 function get_installed_config_version() {
-    cat /opt/hiddify-config/VERSION
+    cat /opt/iranvless-config/VERSION
 }
 
 function get_package_mode() {
-    cd /opt/hiddify-config/hiddify-panel
-    python3 -m hiddifypanel all-configs | jq -r ".hconfigs.package_mode"
+    cd /opt/iranvless-config/iranvless-panel
+    python3 -m iranvlesspanel all-configs | jq -r ".hconfigs.package_mode"
 }
 
 function error() {

fb654422d8c12b220005ebcbafa011a41fbdef15
iranvless <143781323+iranvless@users.noreply.github.com>
2023-09-01 11:35:20 +0330

    Update install.sh

--- haproxy/install.sh
+++ haproxy/install.sh
@@ -1,6 +1,6 @@
-systemctl kill hiddify-sniproxy > /dev/null 2>&1
-systemctl stop hiddify-sniproxy > /dev/null 2>&1
-systemctl disable hiddify-sniproxy > /dev/null 2>&1
+systemctl kill iranvless-sniproxy > /dev/null 2>&1
+systemctl stop iranvless-sniproxy > /dev/null 2>&1
+systemctl disable iranvless-sniproxy > /dev/null 2>&1
 
 systemctl kill haproxy > /dev/null 2>&1
 systemctl stop haproxy > /dev/null 2>&1
@@ -12,7 +12,7 @@ if ! command -v haproxy ;then
     apt update
     apt install haproxy=2.8.\* -y
 fi
-ln -sf $(pwd)/hiddify-haproxy.service /etc/systemd/system/hiddify-haproxy.service
-systemctl enable hiddify-haproxy.service
+ln -sf $(pwd)/iranvless-haproxy.service /etc/systemd/system/iranvless-haproxy.service
+systemctl enable iranvless-haproxy.service
 
 rm haproxy.cfg*

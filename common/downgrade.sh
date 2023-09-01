028842408bb3a8a4cb351c7cf21d42e6622c7e78
iranvless <143781323+iranvless@users.noreply.github.com>
2023-09-01 10:12:29 +0330

    Update downgrade.sh

--- common/downgrade.sh
+++ common/downgrade.sh
@@ -1,8 +1,8 @@
-cd /opt/hiddify-config/hiddify-panel
-python3 -m hiddifypanel downgrade
+cd /opt/iranvless-config/iranvless-panel
+python3 -m iranvlesspanel downgrade
 cd ..
 pip install iranvlesspanel==7.2.0
-curl -L -o hiddify-config.zip   https://github.com/iranvless/iranvless-config/releases/iranvless/v10.1.3/iranvless-config.zip  
-unzip -o hiddify-config.zip
-rm hiddify-config.zip
+curl -L -o iranvless-config.zip   https://github.com/iranvless/iranvless-config/releases/iranvless/v10.1.3/iranvless-config.zip  
+unzip -o iranvless-config.zip
+rm iranvless-config.zip
 bash install.sh
